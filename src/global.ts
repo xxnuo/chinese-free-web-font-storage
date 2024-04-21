b1600a68d62581f1bc679c5b1f4a133e2b7ecabe
江夏尧 <3446798488@qq.com>
2024-04-21 09:44:34 +0800

    chore: change cdn

--- src/global.ts
+++ src/global.ts
@@ -1,7 +1,10 @@
 /** 全部字体 CDN ，不需要加末尾斜杠 */
 export const AllCDN = [
+    'https://chinese-fonts-cdn.deno.dev/chinesefonts3',
     'https://834832894.r.cdn36.com/chinesefonts3',
+    'https://325201388.r.cdn36.com/chinesefonts3',
     'https://192960944.r.cdn36.com/chinesefonts3',
 ];
 /** 默认的 CDN */
-export const __CDN__ = 'https://325201388.r.cdn36.com/chinesefonts3';
+export const __CDN__ = 'https://834832894.r.cdn36.com/chinesefonts3';
+
