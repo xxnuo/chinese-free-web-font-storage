2005da4e38ad3dae71a91418ad8f620dd55eddbb
江夏尧 <3446798488@qq.com>
2024-06-16 13:57:39 +0800

    feat: 添加 vercel 地址

--- src/global.ts
+++ src/global.ts
@@ -1,4 +1,7 @@
 /** 全部字体 CDN ，不需要加末尾斜杠 */
-export const AllCDN = ['https://chinese-fonts-cdn.deno.dev/chinesefonts3'];
+export const AllCDN = [
+    'https://chinese-fonts-cdn.deno.dev/chinesefonts3',
+    'https://chinese-fonts-cdn.vercel.app',
+];
 /** 默认的 CDN */
 export const __CDN__ = 'https://chinese-fonts-cdn.netlify.app';
